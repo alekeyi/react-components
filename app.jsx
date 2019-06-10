48a6537d693c5a5f98edf6ae3a2d7ace93eb37d6
alekeyi <aleksey.ioseliani@gmail.com>
2019-06-10 16:48:57 -0500

    implemented new features

--- app.jsx
+++ app.jsx
@@ -9,9 +9,9 @@ const App = (props) => (
 
 const GroceryList = () => {
   const items = ['food', 'drugs', 'rocknroll'];
-  const list = items.map(item => {
+  const list = items.map((item, index) => {
     return(
-      <GroceryListItem item = {item} />
+      <GroceryListItem item = {item} key={index} />
     )
   })
 
@@ -26,17 +26,36 @@ const GroceryList = () => {
 
 class GroceryListItem extends React.Component {
   constructor(props) {
-    super(props);   
+    super(props);
+    this.state = {
+      done: false,
+      hover: false
+    };
+  }
+  toggleHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+    console.log('hovered')
+  }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+    console.log("Click")
   }
 
   render(props) {
       // console.log(item);
+      let style = {
+        textDecoration: this.state.done ? 'line-through' : 'none',
+        fontWeight: this.state.hover ? 'bold' : 'normal'
+      }
       return (
-
-          <ul>
+          <ul style={style} onClick={this.onListItemClick.bind(this)} onMouseEnter= {this.toggleHover.bind(this)} onMouseLeave= {this.toggleHover.bind(this)}>
             {this.props.item}
           </ul>
-      )
+      );
   }
 }
 
